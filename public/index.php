b2bb5253e008b5488bb9e605ae64cdd74f7a35fd
Nicolas Klaumann <nicolas@conectra.com.br>
2025-09-17 23:59:54 -0300

    feat: refaz logica de roteamento

--- public/index.php
+++ public/index.php
@@ -9,70 +9,87 @@ use App\Controller\ControllerContato;
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Remove o /index.php da URI se ele estiver presente
 $uri = str_replace('/index.php', '', $uri);
 
-// Router simples
-switch (true) {
-    // Pessoa
-    case $uri === '/' || $uri === '/pessoa':
-        (new ControllerPessoa($entityManager))->index();
-        break;
+// Normaliza a URI: remove a barra final para que /recurso/ e /recurso sejam tratados da mesma forma
+if ($uri !== '/') {
+    $uri = rtrim($uri, '/');
+}
 
-    case $uri === '/pessoa/create' && $method === 'GET':
-        (new ControllerPessoa($entityManager))->create();
-        break;
+// Defiição das rotas
+$routes = [
+    'pessoa' => [
+        'GET'    => [
+            ''  => [ControllerPessoa::class, 'index'],
+            '/' => [ControllerPessoa::class, 'index'],
+            '/create'  => [ControllerPessoa::class, 'create'],
+            '/(?<id>\d+)' => [ControllerPessoa::class, 'show'],
+            '/(?<id>\d+)/edit' => [ControllerPessoa::class, 'edit'],
+            '/(?<id>\d+)/delete' => [ControllerPessoa::class, 'delete'],
+        ],
+        'POST' => [
+            '/store'    => [ControllerPessoa::class, 'store'],
+            '/(?<id>\d+)/update' => [ControllerPessoa::class, 'update'],
+        ],
+    ],
+    'contato' => [
+        'GET'    => [
+            ''  => [ControllerContato::class, 'index'],
+            '/' => [ControllerContato::class, 'index'],
+            '/create'  => [ControllerContato::class, 'create'],
+            '/(?<id>\d+)' => [ControllerContato::class, 'show'],
+            '/(?<id>\d+)/edit' => [ControllerContato::class, 'edit'],
+            '/(?<id>\d+)/delete' => [ControllerContato::class, 'delete'],
+        ],
+        'POST' => [
+            '/store'    => [ControllerContato::class, 'store'],
+            '/(?<id>\d+)/update' => [ControllerContato::class, 'update'],
+        ],
+    ]
+];
 
-    case $uri === '/pessoa/store' && $method === 'POST':
-        (new ControllerPessoa($entityManager))->store($_POST);
-        break;
+// Lógica de roteamento
+$pathParts = explode('/', trim($uri, '/'));
+$resource = $pathParts[0] ?? '';
 
-    case preg_match('#^/pessoa/(\d+)$#', $uri, $matches) && $method === 'GET':
-        (new ControllerPessoa($entityManager))->show((int)$matches[1]);
-        break;
-        
-    case preg_match('#^/pessoa/(\d+)/edit$#', $uri, $matches) && $method === 'GET':
-        (new ControllerPessoa($entityManager))->edit((int)$matches[1]);
-        break;    
- 
-    case preg_match('#^/pessoa/(\d+)/update$#', $uri, $matches) && $method === 'POST':
-        (new ControllerPessoa($entityManager))->update((int)$matches[1], $_POST);
-        break;
+// Redireciona a raiz para a página de pessoas
+if ($resource === '') {
+    header('Location: /pessoa');
+    exit;
+}
 
-    case preg_match('#^/pessoa/(\d+)/delete$#', $uri, $matches) && $method === 'GET':
-        (new ControllerPessoa($entityManager))->delete((int)$matches[1]);
-        break;
-        
-    // Contatos
-    case $uri === '/contato':
-        (new ControllerContato($entityManager))->index();
-        break;
+if (isset($routes[$resource])) {
+    $resourceRoutes = $routes[$resource];
+    if (isset($resourceRoutes[$method])) {
+        $methodRoutes = $resourceRoutes[$method];
+        $matched = false;
+        foreach ($methodRoutes as $pattern => $handler) {
+            $regex = '#^/' . $resource . $pattern . '$#';
+            if (preg_match($regex, $uri, $matches)) {
+                $controllerClass = $handler[0];
+                $actionMethod = $handler[1];
 
-    case $uri === '/contato/create' && $method === 'GET':
-        (new ControllerContato($entityManager))->create();
-        break;
-
-    case $uri === '/contato/store' && $method === 'POST':
-        (new ControllerContato($entityManager))->store($_POST);
-        break;
-    
-    case preg_match('#^/contato/(\d+)$#', $uri, $matches) && $method === 'GET':
-        (new ControllerContato($entityManager))->show((int)$matches[1]);
-        break;
-
-    case preg_match('#^/contato/(\d+)/edit$#', $uri, $matches) && $method === 'GET':
-        (new ControllerContato($entityManager))->edit((int)$matches[1]);
-        break;
-
-    case preg_match('#^/contato/(\d+)/update$#', $uri, $matches) && $method === 'POST':
-        (new ControllerContato($entityManager))->update((int)$matches[1], $_POST);
-        break;
-
-    case preg_match('#^/contato/(\d+)/delete$#', $uri, $matches) && $method === 'GET':
-        (new ControllerContato($entityManager))->delete((int)$matches[1]);
-        break;
-
-    default:
-        http_response_code(404);
-        echo "<h1>404 - Página não encontrada</h1>";
-        break;
+                $controller = new $controllerClass($entityManager);
+                
+                // Extrai parâmetros nomeados da URL (ex: id)
+                $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+                
+                // Chama o método do controlador com os parâmetros
+                call_user_func_array([$controller, $actionMethod], $params);
+                $matched = true;
+                break;
+            }
+        }
+        if (!$matched) {
+            http_response_code(404);
+            echo "<h1>404 - Página não encontrada</h1>";
+        }
+    } else {
+        http_response_code(405);
+        echo "<h1>405 - Método não permitido</h1>";
+    }
+} else {
+    http_response_code(404);
+    echo "<h1>404 - Página não encontrada</h1>";
 }
